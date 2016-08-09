29d9c4637f8da062431a37aa4af49c3c5290b08f
root <root@ip-172-31-32-207.ec2.internal>
2016-08-09 13:10:08 +0000

    updating the questions and testing push

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+var QUESTIONS = [
+		 {
+		 'id' : 1,
+		 'text' : "When did you first start using a computer? What sort of help did you have when you first used a computer?",
+		 'hint' : "Manual QA testing is a unique job in the sense that it requires the tester to not only have strong domain knowledge, but also be intimately familiar with computers. Hackers who grew up with computers will generally outperform engineers who started using computers only in their late teens or college. This is the reason why many developers like to ask the interviewee questions about his or her computer usage habits. The assumption is that the earlier you started using a computer (and the less help you had in using it), the better practical understanding you would have of how computers actually work. After all, anybody can get a degree from a college; only a few have the experience and the passion to be standout testers."
+		 },
+		 {
+		 'id' : 2,
+		 'text' : "What are your hobbies and interests?",
+		 'hint' : "<p>A. Yet another question that has nothing to do with testing, but keeps popping up in virtually every interview. One of the key principles of the school of Context Driven Testing that has increased in popularity with some recruiters recently is that your hobbies and skills are transferable qualities that affect how you work. Put simply, the modern organization does not merely want someone who can test out applications; it wants people who are passionate about software, computers and how systems work in general.</p><p>Hobbies and interests, therefore, become a way to know about what really interests and motivates you.</p><p>As you can imagine, there is no ‘right’ answer to this question. What the interviewer is really looking for is the passion and depth of knowledge you show for your hobby. It also helps to have hobbies that involve working with complex systems. This course on succeeding in interviews will give you just the right pointers on how to talk about anything – including your hobbies and interests.</p>"
+		 },
+		 {
+		 'id' : 3,
+		 'text' : "How would you define a ‘good tester’?",
+		 'hint' : "Good testers are people who have an inherent knack for taking things apart. They are natural tinkerers and hackers who will find all possible ways to break apart any software. A good tester will find hundreds of test cases for testing a simple electronic device (like a microwave oven). Good testers also have the ability to work in a team and communicate freely and frequently with the developers."
+		 },
+		 {
+		 'id' : 4,
+		 'text' : "Can you act like an inexperienced user?",
+		 'hint' : "This rather tricky question is one of the favorites among developers simply because so many QA testers slip up on it. Since the job of the tester is to find errors and bugs in the program, being able to work and think like an inexperienced user is a huge bonus. But any competent QA tester is so used to working with technology that he cannot be reasonably to expected to think like a beginner. So when you get a question like this, don’t answer with an emphatic “yes!”. Instead, say that while you can’t think like an inexperienced user, you can definitely write test plans to approximate such a user."
+		 },
+		 {
+		 'id' : 5,
+		 'text' : "What is your process for QA testing? OR What was the process for QA testing in the last company you worked with?",
+		 'hint' : "QA testing is the process of following processes. Great testers are anything but not methodical. When an interviewer asks you this question, he expects you to tell him or her – in detail – the process you follow(ed) when testing software. A sample answer would run the interviewer through the entire software development process – from the creation of the business requirement document to the development of the test plan and the actual testing (both manual and automatic)."
+		 },
+		 {
+		 'id' : 6,
+		 'text' : "How would you define a ‘Test Plan’? What all should a test plan contain?",
+		 'hint' : "<p>A test plan is the road map according to which all QA testers must work. It is a high level document that explains in detail, the testing strategy, expected delivery time, and the type of resources available to the tester.</p><p>Most test plans can be broken down into the following constituents: a. Objectives of the test b. Strategy adopted to fulfill the test requirements c. Resources available to complete the test d. Entry criteria e. Exit criteria f. What must be tested/what must be skipped g. Risks involved in the testing process, and how can they be circumvented.</p>"
+		 },
+		 {
+		 'id' : 7,
+		 'text' : "Explain the difference between Retesting and Data Driven Testing?",
+		 'hint' : "Retesting is the process of testing any software application before bug fixes, changes and enhancements. It is done manually and thus, is more time consuming (but also more insightful) than Data Driven Testing. Data Driven Testing, also called DDT, describes an automated testing process wherein multiple data sets are used to test a feature."
+		 },
+		 {
+		 'id' : 8,
+		 'text' : "How would you write a good bug report OR What all must you tell the developer to fix a bug?",
+		 'hint' : "<p>Never underestimate the value of good communication skills. Generating bug reports is a huge part of any tester’s job. Being able to create concise, easily readable bug reports is essential to be done in timely manner.</p>"
+		 },
+		 {
+		 'id' : 12,
+		 'text' : "How would you test a toaster?",
+		 'hint' : "<p>Another staple of QA testing interviews, this question is designed to see how you approach a problem. The interviewer might replace toaster withrun on battery backup, etc.</p>"
+		 },
+		 {
+		 'id' : 13,
+		 'text' : "How much do you know our product?",
+		 'hint' : ""
+		 },
+		 {
+		 'id' : 14,
+		 'text' : "What is Selenium?",
+		 'hint' : "Selenium automates browsers. That's it! What you do with that power is entirely up to you. Primarily, it is for automating web applications for testing purposes, but is certainly not limited to just that. Boring web-based administration tasks can (and should!) also be automated as well."
+		 },
+		 {
+		 'id' : 15,
+		 'text' : "How would you deal with disagreement in software being ready for production release.",
+		 'hint' : "Assert the facts as I see them; look for consensus amongst the team; secure final decision from product manager. Communicate known issues with customers."
+		 },
+		 {
+		 'id' : 16,
+		 'text' : "What kind of document you think is a good one?",
+		 'hint' : "Clear and logical."
+		 },
+		 {
+		 'id' : 17,
+		 'text' : "What is the difference between priority and severity? Give examples of each.",
+		 'hint' : "Severity defines the extent of the damage caused by a defect, while priority defines the order in which the defect should be solved. A problem that causes the entire application to crash when a user performs a very rare sequence of steps has high severity (since the app crashes altogether), but low priority (since the defect is accessible to very few users). On the other hand, a defect that causes a specific, scarcely used function to become non-usable when a user performs a common action has low severity (only a part of the application stops working) but high priority (it’s affecting a large number of users)."
+		 },
+		 {
+		 'id' : 18,
+		 'text' : "What is the role of documentation in QA?",
+		 'hint' : "Documentation plays a critical role in QA. QA practices should be documented, so that they are repeatable. Specifications, designs, business rules, inspection reports, configurations, code changes, test plans, test cases, bug reports, user manuals should all be documented. Ideally, there should be a system for easily finding and obtaining of documents and determining what document will have a particular piece of information. Use documentation change management, if possible."
+		 },
+		 {
+		 'id' : 19,
+		 'text' : "What is the Software Development Life Cycle?",
+		 'hint' : "The systems (or software) development life cycle (SDLC) is a conceptual model used in project management that describes the stages involved in an information system development project, from an initial feasibility study through maintenance of the completed application. It includes the following different stages: 1. Requirement phase 2. Design phase 3. Coding (programming) 4. Testing 5. Release (Production) 6. Maintenance (Support)"
+		 }
+]
---
+++ js/questions2.js
@@ -0,0 +1,45 @@
+var QUESTIONS = [
+	{
+		'id' : 1,
+		'text' : 'What is metrology?',
+		'hint' : 'Metrology is the science of measurement and includes all theoretical and practical aspects of measurement.'
+	},
+	{
+		'id' : 2,
+		'text' : 'What do you think would be the biggest challenge in this role?',
+		'hint' : 'This is a variation on the "What\'s your greatest weakness?" question as the idea here is to identify what aspect within the company will be the hardest part to handle. Another way to word this is, "What part of this job will be the most difficult for you?" Whatever you pick as the item isn\'t the key point as much as it is what kind of follow-up do you give to it. For example, if writing documentation was a great challenge then one may answer that, "Going back to document what was done and why it was done is often a challenge as there are usually other top priority tasks to get done yet I do try to set aside some of my time to ensure that things are written down in a wiki or something that is accessible to everyone on the team," so that while the documentation is the challenge, there are things being done about it. Some people may pick that they are very diligent about doing good work and may have issues with perfectionistic tendencies.'
+	},
+	{
+		'id' : 3,
+		'text' : 'How would you sell me this pen?',
+		'hint' : 'Find out how they last used a pen (gather info)<br/>Emphasize the importance of the activity they last used a pen (respond to info)<br/>Sell something bigger than a pen, like a state of mind (deliver info)<br/>Ask for the buy (closing)'
+	},
+	{
+		'id' : 4,
+		'text' : 'Describe a experience where you had to tell some one about a product.'
+	},
+	{
+		'id' : 5,
+		'text' : 'What is a typical sales call for you?'	
+	},
+	{
+		'id' : 6,
+		'text' : 'What do you do outside of work to better yourself?'
+	},
+	{
+		'id' : 7,
+		'text' : 'How would you handle a differing opinion from another experienced Engineer?'	
+	},
+	{
+		'id' : 8,
+		'text' : 'How do you manage change?'
+	},
+	{
+		'id' : 9,
+		'text' : 'Could you provide an overview of the sales process cycle?'	
+	},
+	{
+		'id' : 10,
+		'text' : 'How did you resolve a business problem with no clear solution?'
+	},
+]
